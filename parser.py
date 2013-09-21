b9d90a5cdae13c9ebedda4e05ca5b5332390ef33
Windom <wndmearle@yahoo.com>
2013-09-21 17:22:36 +0300

    conform to PEP8

--- parser.py
+++ parser.py
@@ -1,9 +1,12 @@
-from pyparsing import Forward, Word, alphas, Suppress, OneOrMore, Group
-from pyparsing import ZeroOrMore, Keyword, delimitedList, MatchFirst, ParserElement
+from pyparsing import \
+    Forward, Word, alphas, Suppress, OneOrMore, Group, \
+    ZeroOrMore, Keyword, delimitedList, MatchFirst, ParserElement
 from ast import Variable, Abstraction, Application, Definition, LetExpression
+
 
 def SuppressedKeyword(literal):
     return Keyword(literal).suppress()
+
 
 def makeApplications(toks):
     expr = toks.pop(0)
@@ -11,28 +14,35 @@ def makeApplications(toks):
         expr = Application(expr, tok)
     return expr
 
+
 LET, IN = map(SuppressedKeyword, ["let", "in"])
 LMB, ABSTR, EQ, LPAR, RPAR = map(Suppress, ["\\", "->", "=", "(", ")"])
 keyword = MatchFirst([LET, IN])
 
-variable = (~keyword + Word(alphas)).addParseAction(lambda s, loc, toks: Variable(toks[0]))
+variable = (~keyword + Word(alphas)).addParseAction(
+    lambda s, loc, toks: Variable(toks[0]))
 
 expression = Forward()
 
 abstraction = (LMB + Group(OneOrMore(variable)) + ABSTR + expression) \
-        .addParseAction(lambda s, loc, toks: Abstraction(list(toks[0]), toks[1]))
+    .addParseAction(lambda s, loc, toks: Abstraction(list(toks[0]), toks[1]))
 
 atom = variable | abstraction | (LPAR + expression + RPAR)
 
-smpexpression = OneOrMore(atom).addParseAction(lambda s, loc, toks: makeApplications(toks))
+smpexpression = OneOrMore(atom).addParseAction(
+    lambda s, loc, toks: makeApplications(toks))
 
 definition = (variable + Group(ZeroOrMore(variable)) + EQ + expression) \
-        .addParseAction(lambda s, loc, toks: Definition(toks[0], list(toks[1]), toks[2]))
+    .addParseAction(
+        lambda s, loc, toks: Definition(toks[0], list(toks[1]), toks[2]))
 
-letexpression = (LET + Group(delimitedList(definition, delim=",")) + IN + expression) \
-        .addParseAction(lambda s, loc, toks: LetExpression(list(toks[0]), toks[1]))
+definitions = Group(delimitedList(definition, delim=","))
+
+letexpression = (LET + definitions + IN + expression) \
+    .addParseAction(lambda s, loc, toks: LetExpression(list(toks[0]), toks[1]))
 
 expression << (letexpression | smpexpression)
+
 
 def parse(s):
     return expression.parseString(s, parseAll=True)[0]
